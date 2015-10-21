420bcc62a5c9f15c3b687762661269e3d0af40f3
Jonathan Landon <jlandon@me.com>
2015-10-21 09:07:31 -0500

    convert project to a dynamic framework

---
+++ CocoaMQTT/CocoaMQTT.h
@@ -0,0 +1,20 @@
+//
+//  CocoaMQTT.h
+//  CocoaMQTT
+//
+//  Created by Jonathan Landon on 10/21/15.
+//  Copyright © 2015 slimpp.io. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CocoaMQTT.
+FOUNDATION_EXPORT double CocoaMQTTVersionNumber;
+
+//! Project version string for CocoaMQTT.
+FOUNDATION_EXPORT const unsigned char CocoaMQTTVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CocoaMQTT/PublicHeader.h>
+#import "MSWeakTimer.h"
+#import "GCDAsyncSocket.h"
+
